17d5ad1681e92ba333de62d6e32fe11c728c5cc4
Matthias Diester <mddiester@gmail.com>
2018-08-14 13:44:03 +0200

    Simplify package script
    Remove explicit naming of the binaries and simplify loop that handles
    compiliation of tools.

--- scripts/package.sh
+++ scripts/package.sh
@@ -14,22 +14,14 @@ while read -r OS ARCH; do
   TARGET_PATH="${BASEDIR}/binaries/${OS}-${ARCH}"
   mkdir -p "$TARGET_PATH"
 
-  while read -r BINARY PKGPATH; do
+  for PKGPATH in golang.org/x/lint/golint github.com/fzipp/gocyclo honnef.co/go/tools/cmd/megacheck github.com/client9/misspell/cmd/misspell mvdan.cc/sh/cmd/shfmt; do
     VERSION=$(cd "$GOPATH/src/${PKGPATH}" && (git describe --tags 2>/dev/null || git rev-parse HEAD | cut -c1-8))
-    TARGET_FILE="${TARGET_PATH}/${BINARY}"
 
-    echo -e "Compiling \\033[1m${BINARY}\\033[0m version \\033[1;3m${VERSION}\\033[0m for OS \\033[1;34m${OS}\\033[0m and architecture \\033[1;33m${ARCH}\\033[0m"
-    GOOS="$OS" GOARCH="$ARCH" go build -ldflags="-s -w" -o "$TARGET_FILE" "${PKGPATH}"
+    echo -e "Compiling \\033[1m${PKGPATH}\\033[0m version \\033[1;3m${VERSION}\\033[0m for OS \\033[1;34m${OS}\\033[0m and architecture \\033[1;33m${ARCH}\\033[0m"
+    (cd "${TARGET_PATH}" && GOOS="$OS" GOARCH="$ARCH" go build -ldflags="-s -w" "${PKGPATH}")
+  done
 
-  done <<EOB
-golint     golang.org/x/lint/golint
-gocyclo    github.com/fzipp/gocyclo
-megacheck  honnef.co/go/tools/cmd/megacheck
-misspell   github.com/client9/misspell/cmd/misspell
-shfmt      mvdan.cc/sh/cmd/shfmt
-EOB
-
-  tar -cf - -C "${TARGET_PATH}" golint gocyclo megacheck misspell shfmt | gzip --best >"${OUTPUT_DIR}/golang-dev-tools-${OS}-${ARCH}.tar.gz"
+  (cd "${TARGET_PATH}" && tar -cf - -- *) | gzip --best >"${OUTPUT_DIR}/golang-dev-tools-${OS}-${ARCH}.tar.gz"
 
 done <<EOL
 darwin	 amd64
