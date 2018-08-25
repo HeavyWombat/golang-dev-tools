ac0165565d7d702a34203643202674f2a4284efa
Matthias Diester <mddiester@gmail.com>
2018-08-26 00:24:03 +0200

    Enforce static linked binaries
    Add options to go build to enforce static linked binaries.

--- scripts/package.sh
+++ scripts/package.sh
@@ -24,7 +24,7 @@ while read -r OS ARCH; do
     VERSION=$(cd "$GOPATH/src/${PKGPATH}" && (git describe --abbrev=0 --tags 2>/dev/null || git rev-parse HEAD | cut -c1-8))
 
     echo -e "Compiling \\033[1m${PKGPATH}\\033[0m version \\033[1;3m${VERSION}\\033[0m for OS \\033[1;34m${OS}\\033[0m and architecture \\033[1;33m${ARCH}\\033[0m"
-    (cd "${TARGET_PATH}" && GOOS="$OS" GOARCH="$ARCH" go build -ldflags="-s -w" "${PKGPATH}")
+    (cd "${TARGET_PATH}" && GOOS="$OS" GOARCH="$ARCH" go build -a -ldflags='-s -w -extldflags "-static"' "${PKGPATH}")
   done
 
   echo -e "Package binaries into tarball \\033[1;32m${OUTPUT_DIR}/golang-dev-tools-${OS}-${ARCH}.tar.gz\\033[0m"
