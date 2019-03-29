0b9dcac8b0fa5d9138eaf45a253ae0c69859b04b
Matthias Diester <mddiester@gmail.com>
2019-03-29 21:22:37 +0100

    Move jq check away from main flow
    Check for jq pre-req only if it is about to be used.

--- scripts/download-latest.sh
+++ scripts/download-latest.sh
@@ -27,17 +27,17 @@ if ! hash curl 2>/dev/null; then
   exit 1
 fi
 
-if ! hash jq 2>/dev/null; then
-  echo -e 'Required tool \033[1mjq\033[0m is not installed.'
-  exit 1
-fi
-
 if [[ "$(uname -m)" != "x86_64" ]]; then
   echo -e "Unsupported machine type \\033[1m$(uname -m)\\033[0m: Please check \\033[4;94mhttps://api.github.com/repos/HeavyWombat/golang-dev-tools/releases\\033[0m manually"
   exit 1
 fi
 
 if [[ $# -eq 0 ]]; then
+  if ! hash jq 2>/dev/null; then
+    echo -e 'Required tool \033[1mjq\033[0m is not installed.'
+    exit 1
+  fi
+
   # Find the latest golang-dev-tools version using the GitHub API
   SELECTED_TAG="$(curl --silent --location https://api.github.com/repos/HeavyWombat/golang-dev-tools/releases | jq --raw-output '.[0].tag_name')"
 else
